24db188f027ac8ee57ad7d8bd45ee7443de4cc37
Sihayah <sihaya_a@hotmail.com>
2022-02-11 04:55:03 -0500

    created friend routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -59,7 +59,31 @@ const userController = {
                 res.json(dbUserData)
                 .catch(err => res.status(400).json(err));
             })
+    },
+    addFriend({ params, body }, res) {
+        User.findByIdAndUpdate(
+            { _id: params.id },
+            {$push: { friends: body } },
+            { new: true }
+            )
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({ message: 'No User found with this id'});
+                    return;
+                }
+                res.json(dbUserData)
+            })
+            .catch(err => res.json(err));
+    },
+    deleteFriend({ params }, res) {
+        User.findByIdAndUpdate(
+            { _id: params.id },
+            { $pull: { friends: params.friendId }},
+            { new: true }
+        )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
     }
-}
+};
 
 module.exports = userController;
