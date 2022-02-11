f06859585f1c759781c50fc9614238ebc898c096
Sihayah <sihaya_a@hotmail.com>
2022-02-11 02:39:26 -0500

    added Reactions as subdocument to Thoughts model

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,6 +1,34 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ReactionsSchema = (
+    {
+        reactionId: {
+            type: Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtValue => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJson: {
+            getters: true
+        }
+    }
+)
+
 const ThoughtsSchema = (
     {
         thoughtText: {
@@ -18,7 +46,7 @@ const ThoughtsSchema = (
             type: String,
             required: true
         }, 
-        reactions: [{ type: Schema.Types.ObjectId, ref: 'Reactions'}]
+        reactions: [ReactionsSchema]
     }
 );
 
