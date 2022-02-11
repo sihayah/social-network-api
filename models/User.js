709d7b4035ba3f3e70b683403bbaa603a62ba576
Sihayah <sihaya_a@hotmail.com>
2022-02-11 02:10:14 -0500

    updated models file name, create User model

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const { Schema, model, Types } = require('mongoose');
+
+const UserSchema = new Schema (
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /^\S+@\S+\.\S+$/
+        },
+        thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thoughts'}],
+        friends: [{ type: Schema.Types.ObjectId, ref: 'User'}],
+
+    }
+);
+
+UserSchema.virtual('friendCount').get( function() {
+    return this.friends.length
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
